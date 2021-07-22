63bc303892aaeb7f23473170c16b323ada9964f4
shravaniha <70507796+shravaniha@users.noreply.github.com>
2021-07-22 17:03:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function ComputeInterest()
+            {
+                p = document.getElementById("p").value;
+                n = document.getElementById("n").value;
+                r = document.getElementById("r").value;
+                q=((p*n*r)/100)
+                s=2020+Number(n);
+                result = document.getElementById("result");   
+                result.innerHTML = "If you deposit " + p + " at an interest rate of " + r + " You will receive an amount " + q + " in the year "+s ;  
+            }  
