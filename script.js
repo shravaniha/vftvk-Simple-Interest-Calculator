e44a3de5bf05c87d6fcce92753f477d54c4c82c3
shravaniha <70507796+shravaniha@users.noreply.github.com>
2021-08-18 11:36:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-function ComputeInterest()
-            {
-                p = document.getElementById("p").value;
-                n = document.getElementById("n").value;
-                r = document.getElementById("r").value;
-                q=((p*n*r)/100)
-                s=2020+Number(n);
-                result = document.getElementById("result");   
-                result.innerHTML = "If you deposit " + p + " at an interest rate of " + r + " You will receive an amount " + q + " in the year "+s ;  
-            }  
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+    
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
+}
+
+function SliderValue()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
+}
